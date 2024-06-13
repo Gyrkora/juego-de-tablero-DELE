867b404f88411bfd8354e03a2d691b87f0bc07fd
Gyrkora <giselle.gallardo0210@gmail.com>
2024-06-13 02:52:33 -0300

    fichas responsive y tamaño foto original

--- js/script.js
+++ js/script.js
@@ -1,35 +1,59 @@
 const pieces = document.querySelectorAll('.piece');
 
 pieces.forEach((piece) => {
-	piece.addEventListener('mousedown', onMouseDown);
+    piece.addEventListener('mousedown', onMouseDown);
+    piece.addEventListener('touchstart', onTouchStart);
 
-	function onMouseDown(event) {
-		event.preventDefault();
+    function onMouseDown(event) {
+        event.preventDefault();
 
-		let shiftX = event.clientX - piece.getBoundingClientRect().left;
-		let shiftY = event.clientY - piece.getBoundingClientRect().top;
+        let shiftX = event.clientX - piece.getBoundingClientRect().left;
+        let shiftY = event.clientY - piece.getBoundingClientRect().top;
 
-		piece.style.cursor = 'grabbing';
+        piece.style.cursor = 'grabbing';
 
-		function moveAt(pageX, pageY) {
-			piece.style.left = pageX - shiftX + 'px';
-			piece.style.top = pageY - shiftY + 'px';
-		}
+        function moveAt(pageX, pageY) {
+            piece.style.left = pageX - shiftX + 'px';
+            piece.style.top = pageY - shiftY + 'px';
+        }
 
-		function onMouseMove(event) {
-			moveAt(event.pageX, event.pageY);
-		}
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+        }
 
-		document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mousemove', onMouseMove);
 
-		piece.onmouseup = function () {
-			document.removeEventListener('mousemove', onMouseMove);
-			piece.onmouseup = null;
-			piece.style.cursor = 'grab';
-		};
+        piece.onmouseup = function () {
+            document.removeEventListener('mousemove', onMouseMove);
+            piece.onmouseup = null;
+            piece.style.cursor = 'grab';
+        };
 
-		piece.ondragstart = function () {
-			return false;
-		};
-	}
+        piece.ondragstart = function () {
+            return false;
+        };
+    }
+
+    function onTouchStart(event) {
+        event.preventDefault();
+
+        let shiftX = event.touches[0].clientX - piece.getBoundingClientRect().left;
+        let shiftY = event.touches[0].clientY - piece.getBoundingClientRect().top;
+
+        function moveAt(pageX, pageY) {
+            piece.style.left = pageX - shiftX + 'px';
+            piece.style.top = pageY - shiftY + 'px';
+        }
+
+        function onTouchMove(event) {
+            moveAt(event.touches[0].pageX, event.touches[0].pageY);
+        }
+
+        document.addEventListener('touchmove', onTouchMove);
+
+        piece.ontouchend = function () {
+            document.removeEventListener('touchmove', onTouchMove);
+            piece.ontouchend = null;
+        };
+    }
 });
